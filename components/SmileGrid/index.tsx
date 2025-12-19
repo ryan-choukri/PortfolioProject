a209465dcb81e69e49a2cc52fc3e01588cd05c0c
Ryan <ryan@MacBook-Air-de-Ryan.local>
2025-12-19 03:59:02 +0100

    refacto a bit

--- components/SmileGrid/index.tsx
+++ components/SmileGrid/index.tsx
@@ -14,7 +14,7 @@ type Entity = {
 
 const GRID_SIZE = 100; // taille de la grille en nombre de cellules
 const CELL_SIZE = 6; // taille d'une cellule en pixels
-const NUM_SMILEYS = 110; // nombre initial de smileys
+const NUM_SMILEYS = 20; // nombre initial de smileys
 const SPEED_GAME = 24; // nombre en ms entre chaque déplacement
 const NATALITY = 0.3; // taux de natalité
 const SMILEY_SIZE = 0.2; // taille des smileys
@@ -36,16 +36,17 @@ const MOVE_MAP: Record<Direction, { dx: number; dy: number }> = {
   right: { dx: 1, dy: 0 },
 };
 
-const makeSmiley = (id: number, state: string = 'normal'): Entity => ({
+const makeSmiley = (x: number, y: number, id: number, state: string = 'normal'): Entity => ({
+  //make baby born from two parents positioned at the same place
   id,
-  x: Math.floor(Math.random() * GRID_SIZE),
-  y: Math.floor(Math.random() * GRID_SIZE),
+  x,
+  y,
   dir: DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)],
   state: state,
   smyleyMeet: [],
 });
 
-const generateSmyleys = Array.from({ length: NUM_SMILEYS }, (_, i) => makeSmiley(i, 'normal'));
+const generateSmyleys = Array.from({ length: NUM_SMILEYS }, (_, i) => makeSmiley(Math.floor(Math.random() * GRID_SIZE), Math.floor(Math.random() * GRID_SIZE), i, 'normal'));
 
 // Commente cette focntion EmojiGame explique les interaction et la logique en quelque ligne
 export default function EmojiGame() {
@@ -56,6 +57,13 @@ export default function EmojiGame() {
   const [natality, setNatality] = useState(NATALITY);
   const [smileySize, setSmileySize] = useState(SMILEY_SIZE);
   const [walkStraight, setWalkStraight] = useState(CHANGE_PROB);
+  const [leaveTrace, setLeaveTrace] = useState<Map<string, number[]>[]>([]);
+  const [nbBorn, setNbBorn] = useState(0);
+  const [nbDead, setNbDead] = useState(0);
+  const [mousDisatanceEscape, setMousDisatanceEscape] = useState(10);
+
+  // const [mousePos, setMousePos] = useState<{ x: number; y: number }>({ x: -1, y: -1 });
+  const mousePosRef = useRef<{ x: number; y: number }>({ x: -1, y: -1 });
 
   const hasSameId = (arr: number[], threshold: number): { find: boolean; otherids: number[] } => {
     const count: Record<number, number> = {};
@@ -91,84 +99,98 @@ export default function EmojiGame() {
 
   // ✅ Real-time movement + collision detection
   useEffect(() => {
-    if (!isRunning) return;
+    const moveSmiley = (s: Entity, walkStraight: number, gridSize: number, mousePos: { x: number; y: number }, mousEscapeDistance: number) => {
+      //si le smiley est au bord, il doit forcément changer de direction
+      const distX = s.x - mousePos.x;
+      const distY = s.y - mousePos.y;
+      const distance = Math.sqrt(distX * distX + distY * distY);
+      let newDir: Direction = s.dir;
 
-    const interval = setInterval(() => {
-      setSmileys((prev) => {
-        let newPositions = prev.map((s) => {
-          //si le smiley est au bord, il doit forcément changer de direction
-          if (s.x === 0 || s.x === GRID_SIZE - 1 || s.y === 0 || s.y === GRID_SIZE - 1) {
-            //prend la direction opposé à celle du bord
-            const newDir = getSmartDirectionNoBack(s, GRID_SIZE);
-            const newPos = moveInGrid(s, newDir, GRID_SIZE);
-            return { ...s, ...newPos, dir: newDir };
-          }
-          const newDir = Math.random() > walkStraight ? s.dir : getSmartDirectionNoBack(s, GRID_SIZE);
-          const newPos = moveInGrid(s, newDir, GRID_SIZE);
+      if (distance < mousEscapeDistance) {
+        // s'éloigner de la souris
+        if (Math.abs(distX) > Math.abs(distY)) newDir = distX > 0 ? 'right' : 'left';
+        else newDir = distY > 0 ? 'down' : 'up';
+      } else {
+        if (s.x === 0 || s.x === gridSize - 1 || s.y === 0 || s.y === gridSize - 1) {
+          //prend la direction opposé à celle du bord
+          newDir = getSmartDirectionNoBack(s, gridSize);
+          const newPos = moveInGrid(s, newDir, gridSize);
           return { ...s, ...newPos, dir: newDir };
-        });
-
-        const positionsMap = new Map<string, number[]>();
-        newPositions.forEach((s, idx) => {
-          const key = `${s.x},${s.y}`;
-          if (!positionsMap.has(key)) positionsMap.set(key, []);
-          positionsMap.get(key)!.push(idx);
-        });
-
-        const idsToRemove: number[] = [];
-        const toAdd: Entity[] = [];
-
-        positionsMap.forEach((ids) => {
-          if (ids.length > 1) {
-            let birthDone = false;
-
-            ids.forEach((id) => {
-              if (!newPositions[id]) return;
-
-              const meet = newPositions[id].smyleyMeet;
-              const hasFour = hasSameId(meet, 4);
-              const hasThree = hasSameId(meet, 3);
-              const hasTwo = hasSameId(meet, 2);
-
-              // si 4 rencontres identiques => mort
-              if (hasFour.find) {
-                idsToRemove.push(...hasFour.otherids, newPositions[id].id);
-              } else {
-                // try to make baby if is not young already
-                if (prev.length < 2700 && !birthDone && Math.random() < natality && newPositions[id].state !== 'young') {
-                  toAdd.push(makeSmiley(nextIdRef.current++, 'young'));
-                  birthDone = true;
-                }
-              }
-
-              if (hasThree.find) newPositions[id].state = 'old';
-              else if (hasTwo.find) newPositions[id].state = 'makeKid';
-              else newPositions[id].state = 'touched';
-              newPositions[id].smyleyMeet.push(...ids.filter((i) => i !== id));
-            });
-          }
-        });
-
-        // suppression après calcul
-        newPositions = newPositions.filter((s) => !idsToRemove.includes(s.id));
-
-        // 🔴 DEBUG NAISSANCES (AJOUTE ÇA)
-        if (toAdd.length > 0) {
-          console.log('[BIRTH]', 'natality =', natality, '| births =', toAdd.length, '| total before =', prev.length, '| total after =', newPositions.length + toAdd.length);
         }
 
-        return [...newPositions, ...toAdd];
+        newDir = Math.random() > walkStraight ? s.dir : getSmartDirectionNoBack(s, gridSize);
+      }
+      const newPos = moveInGrid(s, newDir, gridSize);
+
+      return { ...s, ...newPos, dir: newDir };
+    };
+
+    const handleCollisions = (newPositions: Entity[], natality: number, nextIdRef: React.MutableRefObject<number>): { posWithCollision: Entity[]; newPos: Entity[] } => {
+      const positionsMap = new Map<string, number[]>();
+      newPositions.forEach((s, idx) => {
+        const key = `${s.x},${s.y}`;
+        if (!positionsMap.has(key)) positionsMap.set(key, []);
+        positionsMap.get(key)!.push(idx);
+      });
+      setLeaveTrace((prev) => {
+        //retain only last 10 traces
+        if (prev.length >= 5) prev.shift();
+        return [...prev, positionsMap];
+      });
+
+      const idsToRemove: number[] = [];
+      const toAdd: Entity[] = [];
+
+      positionsMap.forEach((ids) => {
+        if (ids.length > 1) {
+          let birthDone = false;
+          ids.forEach((id) => {
+            if (!newPositions[id]) return;
+            const meet = newPositions[id].smyleyMeet;
+            const hasFour = hasSameId(meet, 4);
+            const hasThree = hasSameId(meet, 3);
+            const hasTwo = hasSameId(meet, 2);
+            // si 4 rencontres identiques => mort
+            if (hasFour.find) {
+              setNbDead((n) => n + 1);
+              idsToRemove.push(...hasFour.otherids, newPositions[id].id);
+            } else {
+              // try to make baby if is not young already
+              if (newPositions.length < 1500 && !birthDone && Math.random() < natality && newPositions[id].state !== 'young') {
+                toAdd.push(makeSmiley(Math.floor(Math.random() * GRID_SIZE), Math.floor(Math.random() * GRID_SIZE), nextIdRef.current++, 'young'));
+                setNbBorn((n) => n + 1);
+                birthDone = true;
+              }
+            }
+            if (hasThree.find) newPositions[id].state = 'old';
+            else if (hasTwo.find) newPositions[id].state = 'makeKid';
+            else newPositions[id].state = 'touched';
+            newPositions[id].smyleyMeet.push(...ids.filter((i) => i !== id));
+          });
+        }
+      });
+      // suppression après calcul
+      newPositions = newPositions.filter((s) => !idsToRemove.includes(s.id));
+      return { posWithCollision: newPositions, newPos: toAdd };
+    };
+
+    if (!isRunning) return;
+    const interval = setInterval(() => {
+      setSmileys((prev) => {
+        const newPositions = prev.map((entity) => moveSmiley(entity, walkStraight, GRID_SIZE, mousePosRef.current, mousDisatanceEscape));
+        const { posWithCollision, newPos } = handleCollisions(newPositions, natality, nextIdRef);
+        return [...posWithCollision, ...newPos];
       });
     }, speedOfGame);
 
     return () => clearInterval(interval);
-  }, [isRunning, speedOfGame, natality, walkStraight]);
+  }, [isRunning, speedOfGame, natality, walkStraight, mousDisatanceEscape]);
 
   return (
     <div className="flex w-full items-center justify-center overflow-hidden">
       <div className="flex h-screen w-full text-white">
         {/* Sidebar */}
-        <div className="flex w-[420px] flex-col bg-neutral-800 p-4 text-xs text-neutral-200">
+        <div className="flex w-[590px] flex-col bg-neutral-800 p-4 text-xs text-neutral-200">
           <div className="grid flex-1 grid-cols-[1.7fr_1.3fr] gap-3">
             {/* ==== COL 2 : DESCRIPTION + STATS ==== */}
             <div className="flex flex-col gap-2 pr-1">
@@ -188,31 +210,31 @@ export default function EmojiGame() {
 
               {/* Live stats */}
               <h3 className="text-[10px] font-semibold tracking-wide text-neutral-400 uppercase">Live stats</h3>
-              <div className="grid grid-cols-2 gap-x-3 gap-y-1 rounded bg-neutral-700 p-3 font-mono text-[11px]">
+              <div className="grid grid-cols-2 gap-x-3 gap-y-1 rounded bg-neutral-700 p-3 font-mono text-xs">
                 <div>Population</div>
                 <div className="text-right">{smileys.length}</div>
 
                 <div>Natalité</div>
                 <div className="text-right">{natality}</div>
 
-                <div>Vitesse de la simulation</div>
+                <div>Vitesse simu</div>
                 <div className="text-right">{speedOfGame} ms</div>
 
                 <div>Marche droit</div>
                 <div className="text-right">{walkStraight}</div>
 
-                <div>Grid</div>
-                <div className="text-right">
-                  {GRID_SIZE}×{GRID_SIZE}
-                </div>
+                <div>Nb Naissance</div>
+                <div className="text-right">{nbBorn} </div>
+                <div>Nb Déces</div>
+                <div className="text-right">{nbDead} </div>
               </div>
 
               {/* Debug */}
-              <div className="rounded bg-neutral-900 p-2 font-mono text-[11px] text-neutral-400">
+              {/* <div className="rounded bg-neutral-900 p-2 font-mono text-[11px] text-neutral-400">
                 <p>Collisions: —</p>
                 <p>Births / tick: —</p>
                 <p>Deaths / tick: —</p>
-              </div>
+              </div> */}
             </div>
 
             {/* ==== COL 1 : CONTROLS ==== */}
@@ -239,7 +261,7 @@ export default function EmojiGame() {
                 <label htmlFor="speed" className="mb-0.5 block text-xs font-medium font-semibold text-neutral-100">
                   Vitesse de la simulation
                 </label>
-                <input onChange={(e) => setSpeedOfGame(51 - Number(e.target.value))} type="range" id="speed" min="1" max="50" defaultValue={speedOfGame} className="w-full accent-red-500" />
+                <input onChange={(e) => setSpeedOfGame(51 - Number(e.target.value))} type="range" id="speed" min="1" max="50" defaultValue={speedOfGame} className="w-full accent-sky-400" />
               </div>
 
               {/* Walkt raight */}
@@ -254,7 +276,24 @@ export default function EmojiGame() {
                   min="1"
                   max="50"
                   defaultValue={walkStraight * 100}
-                  className="w-full accent-red-500"
+                  className="w-full accent-pink-400"
+                />
+              </div>
+
+              {/* mousDisatanceEscape, setMousDisatanceEscape */}
+              {/* répulsion de la souris */}
+              <div>
+                <label htmlFor="speed" className="mb-0.5 block text-xs font-medium font-semibold text-neutral-100">
+                  répulsion de la souris
+                </label>
+                <input
+                  onChange={(e) => setMousDisatanceEscape(Number(e.target.value))}
+                  type="range"
+                  id="speed"
+                  min="4"
+                  max="50"
+                  defaultValue={mousDisatanceEscape}
+                  className="w-full accent-blue-500"
                 />
               </div>
 
@@ -270,6 +309,13 @@ export default function EmojiGame() {
         <div className="flex flex-1 items-center justify-center">
           <div className="flex h-[600px] w-[600px] items-center justify-center rounded-md">
             <div
+              onMouseMove={(e) => {
+                const rect = e.currentTarget.getBoundingClientRect();
+                mousePosRef.current = {
+                  x: Math.floor((e.clientX - rect.left) / CELL_SIZE),
+                  y: Math.floor((e.clientY - rect.top) / CELL_SIZE),
+                };
+              }}
               className="relative bg-neutral-900"
               style={{
                 width: GRID_SIZE * CELL_SIZE,
@@ -283,6 +329,34 @@ export default function EmojiGame() {
                   backgroundImage: 'linear-gradient(rgba(255,255,255,0.05) 1px, transparent 1px), linear-gradient(90deg, rgba(255,255,255,0.05) 1px, transparent 1px)',
                 }}
               />
+              {/* traces leave a subltile trace on the map under the smileys
+              and it disappears over time */}
+              {/* its laggy */}
+              {/* {leaveTrace.map((traceMap, index) => (
+                <div key={index}>
+                  {Array.from(traceMap.entries()).map(([key, ids]) => {
+                    console.log(index, key, ids.length);
+                    const [x, y] = key.split(',').map(Number);
+                    // increase opacity with number of smileys by index
+                    const opacity = Math.min(0.1 + ids.length * 0.1, 0.9) * ((index + 1) / leaveTrace.length);
+                    return (
+                      <div
+                        key={key}
+                        className="absolute flex items-center justify-center select-none"
+                        style={{
+                          width: CELL_SIZE,
+                          height: CELL_SIZE,
+                          transform: `translate(${x * CELL_SIZE}px, ${y * CELL_SIZE}px)`,
+                          fontSize: CELL_SIZE * (smileySize * 10),
+                          lineHeight: '1',
+                          opacity: opacity,
+                        }}>
+                        {ids.length > 1 ? SMILEY_STATES['touched'] : SMILEY_STATES['normal']}
+                      </div>
+                    );
+                  })}
+                </div>
+              ))} */}
 
               {/* smileys */}
               {smileys.map((s) => (
